4c00766dcd323dda7182dd6824ddaf0c47ac7a4a
wibembli <bembliwiem8@gmail.com>
2025-02-17 00:45:17 +0100

    main layout

--- front/Wealthwise_app/src/app/app.routes.ts
+++ front/Wealthwise_app/src/app/app.routes.ts
@@ -3,11 +3,18 @@ import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { InfopersoComponent } from './pages/infoperso/infoperso.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent }, // ✅ Vérifier que la route existe
+  { path: 'login', component: LoginComponent }, // ✅ Vérifier que la route existe
   { path: 'register', component: RegisterComponent },
-  { path: 'infoperso', component: InfopersoComponent }
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'infoperso', component: InfopersoComponent }
+    ]
+  },
 ];
 
--- front/Wealthwise_app/src/app/pages/infoperso/infoperso.component.ts
+++ front/Wealthwise_app/src/app/pages/infoperso/infoperso.component.ts
@@ -11,19 +11,5 @@ import {Router, RouterModule} from '@angular/router';
   imports: [CommonModule, RouterModule]  // 🔹 Ajout des modules ici
 })
 export class InfopersoComponent {
-  constructor(private router: Router) {}
-  navLinks = [
-    { path: '/dashboard', label: 'Dashboard', icon: 'fas fa-chart-line' }, // 📊 Représente un tableau de bord
-    { path: '/teams', label: 'Transactions', icon: 'fas fa-exchange-alt' }, // 🔄 Icône pour transactions financières
-    { path: '/employees', label: 'Analyses & Statistiques', icon: 'fas fa-chart-pie' }, // 📈 Graphique circulaire pour statistiques
-    { path: '/projects', label: 'Gestion de Budget', icon: 'fas fa-wallet' }, // 💰 Portefeuille pour budget
-    { path: '/meetings', label: 'Historique', icon: 'fas fa-history' }, // ⏳ Horloge pour historique
-    { path: '/infoperso', label: 'Informations Personnelles', icon: 'fas fa-id-card' }, // 🆔 Carte d'identité pour infos perso
-    { path: '/settings', label: 'Réglages', icon: 'fas fa-cogs' } // ⚙️ Engrenages pour réglages
-  ];
-  logout() {
-    localStorage.removeItem('token'); // Supprime le token JWT
-    this.router.navigate(['/login']); // Redirige vers la page de connexion
-  }
 }
 
