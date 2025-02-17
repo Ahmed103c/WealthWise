84ebef4118390165416a264a7523d9042090d8a1
wibembli <bembliwiem8@gmail.com>
2025-02-17 01:30:59 +0100

    Pages informations Personnelles

--- front/Wealthwise_app/src/app/pages/infoperso/infoperso.component.ts
+++ front/Wealthwise_app/src/app/pages/infoperso/infoperso.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Router, RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 
 @Component({
@@ -8,8 +9,37 @@ import {Router, RouterModule} from '@angular/router';
   standalone: true,  // 🔹 Important pour activer l'import direct
   templateUrl: './infoperso.component.html',
   styleUrls: ['./infoperso.component.scss'],
-  imports: [CommonModule, RouterModule]  // 🔹 Ajout des modules ici
+  imports: [CommonModule, RouterModule,FormsModule]  // 🔹 Ajout des modules ici
 })
 export class InfopersoComponent {
+  user = {
+    fullName: 'Jean Dupont',
+    email: 'jean.dupont@email.com',
+    phone: '+33 6 12 34 56 78',
+    accountType: 'Compte Premium',
+    balance: 15230.75,
+    iban: 'FR76 3000 4000 5000 6000 7890 123',
+    verified: true
+  };
+
+  // Etat des champs en mode édition ou non
+  editMode: { [key: string]: boolean } = {
+    fullName: false,
+    email: false,
+    phone: false
+  };
+
+
+  // Permet de basculer entre mode affichage et édition
+  toggleEdit(field: string) {
+    this.editMode[field] = !this.editMode[field];
+  }
+
+  // Fonction pour sauvegarder les modifications
+  saveChanges() {
+    alert('Modifications enregistrées avec succès !');
+  }
 }
+
+
 
